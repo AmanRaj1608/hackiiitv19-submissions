4473a67ed9d473e69fd99ca8d2f9484613acc954
Aman Raj <archanaamanraj@gmail.com>
2019-10-06 10:16:23 +0530

    Merge Frontend+Backend+ML

--- team#29 - Smart_Communication/home/forms.py
+++ team#29 - Smart_Communication/home/forms.py
@@ -3,18 +3,13 @@ from django import forms
 from .models import *
 # Create your tests here.
 class Productform(forms.ModelForm):
-    
     des = forms.CharField(required=False)
-    
     
     class Meta():
         model=Product
         fields=['des']
 class Productform2(forms.ModelForm):
-    
     des = forms.CharField(required=False)
-    
-    
     class Meta():
         model=Product2
         fields=['des']
--- team#29 - Smart_Communication/home/urls.py
+++ team#29 - Smart_Communication/home/urls.py
@@ -3,10 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    
-    
     path("add/",views.add,name="add"),
     path("guideA/",views.guideA,name="guideA"),
     path("guideB/",views.guideB,name="guideB"),
-
 ]
